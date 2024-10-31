38e8bcaf9c17dd3fdfed621f4784f763b4b179f8
SheilandraZarawiba29 <Sheilandrazarawiba561@gmail.com>
2024-10-31 10:38:13 +0700

    Pertanyaan2 Percobaan3

--- Triangle28.java
+++ Triangle28.java
@@ -6,13 +6,14 @@ public class Triangle28 {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         
-        int i = 0;
+        int i = 1;
         while (i <= N) { 
             int j = 0;
             while (j < i) { 
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
